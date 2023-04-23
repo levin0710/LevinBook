f2c1e59b69d2e67f0d749c57ea74bd5d9cad54f9
levin0710 <94712128+levin0710@users.noreply.github.com>
2023-04-23 15:09:58 -0400

    DONE

--- src/client.js
+++ src/client.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const URL = "https://ugjntjgktczmnlajhxco.supabase.co";
-const API_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVnam50amdrdGN6bW5sYWpoeGNvIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODE2ODQ1MjUsImV4cCI6MTk5NzI2MDUyNX0.rit5iWL1Ftjc2JVR-UiRUNBBMbfa3yQIKTmgx-4WhDs";
+const API_KEY = import.meta.env.API_KEY;
+const URL = import.meta.env.URL;
+
 export const supabase = createClient(URL, API_KEY);
